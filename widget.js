c762c8b0a2a503bb757c60dc7aa3ad1bd53ce9a9
AffonsoGames <44729611+AffonsoGames@users.noreply.github.com>
2023-04-25 21:32:36 -0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -47,7 +47,8 @@ window.addEventListener('load', () => {
         fetch(`https://discord.com/api/guilds/${id}/widget.json`).then(data => {
             data.json().then(data => {
                 //member count
-                 count.innerHTML = `<h6><strong>${data.presence_count - 1} JOGADORES ONLINE</strong></h6>`;
+                // count.innerHTML = `<h6><strong>${data.presence_count - 1} JOGADORES ONLINE</strong></h6>`;
+                 count.innerHTML = `<strong>${data.presence_count - 1}</strong> Members Online`;
                 //join button
                 joinButton.setAttribute('href', data.instant_invite);
                 joinButton.setAttribute('target', '_blank');
