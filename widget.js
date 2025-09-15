314019ddccf63bdaf365c1582cca482cccebe7b7
AffonsoGames <44729611+AffonsoGames@users.noreply.github.com>
2025-09-15 11:31:04 -0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -49,7 +49,7 @@ window.addEventListener('load', () => {
                 //member count
                  count.innerHTML = `<center><strong>       ${data.presence_count - 1} JOGADORES ONLINE NO DISCORD</strong>`;
                 //join button
-                joinButton.setAttribute('href', data.instant_invite);
+                joinButton.setAttribute('href', 'https://discord.gg/dqZKDM2QRC');
                 joinButton.setAttribute('target', '_blank');
                 //users
                 data.members.forEach((user) => {
@@ -82,3 +82,4 @@ function LDColor(color, percent) {
     return (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
 }
 ;
+
